e446d6aca724b25ffa67781a736ea6e0723a0d77
allybiz23 <allisonbizaillion@gmail.com>
2024-02-20 13:27:06 -0600

    end product

---
+++ script.js
@@ -0,0 +1,16 @@
+function submitSupportRequest(formData) {
+    // Code to process and submit the form data
+    // server communication to generate a support ticket
+    // Return the support ticket summary
+    // Example: return generatedSummary;
+}
+function searchSupportTicketsByName(userName) {
+    // Code to search for tickets by user name
+    // server communication to retrieve matching tickets
+    // Example: return matchingTickets;
+}
+function searchSupportTicketsByNumber(ticketNumber) {
+    // Code to search for tickets by ticket number
+    // server communication to retrieve matching tickets
+    // Example: return matchingTickets;
+}
